1021dda08ac3c9c874fcc47ab6194425576c7ad0
T1maty <timamy666@gmail.com>
2022-11-03 21:44:50 +0300

    Add project files.

---
+++ ElasticSearchTutorial/ConnectionString.cs
@@ -0,0 +1,16 @@
+﻿using Elasticsearch.Net;
+
+internal class ConnectionString
+{
+    private SingleNodeConnectionPool pool;
+
+    public ConnectionString(SingleNodeConnectionPool pool)
+    {
+        this.pool = pool;
+    }
+
+    internal object DefaultIndex(string v)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ ElasticSearchTutorial/Program.cs
@@ -0,0 +1,36 @@
+using Elasticsearch.Net;
+using Nest;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var pool = new SingleNodeConnectionPool(new Uri("http://localhost:9200"));
+var settings = new ConnectionString(pool)
+    .DefaultIndex("books");
+var client = new ElasticClient((Uri)settings);
+builder.Services.AddSingleton(client);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
